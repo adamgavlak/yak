3fe67a82bc3059e3371ae1ae24b08d96afe0e28a
Adam Gavlák <adam@adamgavlak.com>
2017-05-19 16:45:31 +0200

    Add board tests

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :wework, Wework.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "gavlak",
+  password: "",
   database: "wework_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
