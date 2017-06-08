7ff1be5fc63ec410938d3cd2c13f8b1fcb496c09
Adam Gavlák <adam@adamgavlak.com>
2017-06-08 21:57:25 +0200

    Add simple Postmark client

---
+++ lib/yak/web/support/postmark.ex
@@ -0,0 +1,28 @@
+defmodule Postmark do
+  import Logger
+
+  @from "Yak <podpora@yak.sk>"
+  @reply_to "Adam Gavlák <adam@yak.sk>"
+
+  @template_ids %{
+    confirmation: 2045901
+  }
+  
+  def deliver(template, email, model \\ %{}) do
+    data = %{From: @from,
+      To: email,
+      ReplyTo: @reply_to,
+      TemplateId: @template_ids[template],
+      TemplateModel: model
+    }
+
+    Task.start(fn -> 
+      response = HTTPoison.post(
+        "https://api.postmarkapp.com/email/withTemplate",
+        Poison.encode!(data),
+        [{"Accept", "application/json"}, {"Content-Type", "application/json"}, {"X-Postmark-Server-Token", System.get_env("POSTMARK_API_KEY")}]
+      )
+    end)
+  end
+
+end
