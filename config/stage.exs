3ecf198fdeb43b68487db688e49fa02c9e957107
Adam Gavlák <adam@adamgavlak.com>
2017-06-09 18:52:51 +0200

    Add files for Heroku deployment

---
+++ config/stage.exs
@@ -0,0 +1,18 @@
+use Mix.Config
+
+config :yak, Yak.Web.Endpoint,
+  on_init: {Yak.Web.Endpoint, :load_from_system_env, []},
+  http: [port: {:system, "PORT"}],
+  url: [host: "stage.yak.sk", port: 80],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+config :yak, Yak.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+  
+config :yak, Yak.Mailer,
+  adapter: ExPostmark.Adapters.Postmark,
+  server_api_key: System.get_env("POSTMARK_API_KEY")
